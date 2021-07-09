a25822334d395c470ade3b2e94ff51252bce7c05
cn8817 <christinenguyen1433@gmail.com>
2021-07-08 17:04:08 -0700

    completed all steps in github usercard

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -4,14 +4,19 @@ import axios from 'axios'
     (replacing the placeholder with your Github name):
     https://api.github.com/users/<your name>
 */
-
+const cards = document.querySelector('.cards')
 axios.get('https://api.github.com/users/cn8817')
-  .then(res => {
-    usercard(res.data)
-    console.log(res.data)
-  })
-  .catch(err => console.log(err.message))
-  .finally(() => console.log('done'))
+.then(res => {
+  console.log(res.data)
+  const newCard = usercard(res.data)
+  console.log(newCard)
+  return newCard
+})
+.then(newCard => {
+  cards.appendChild(newCard)
+})
+.catch(err => console.log(err.message))
+.finally(() => console.log('done'))
 
 /*
   STEP 2: Inspect and study the data coming back, this is YOUR
@@ -25,9 +30,6 @@ axios.get('https://api.github.com/users/cn8817')
   STEP 4: Pass the data received from Github into your function,
     and append the returned markup to the DOM as a child of .cards
 */
-const cards = document.querySelector('.cards')
-const induce = usercard()
-cards.appendChild(induce)
 
 /*
   STEP 5: Now that you have your own card getting added to the DOM, either
@@ -40,8 +42,32 @@ cards.appendChild(induce)
     user, and adding that card to the DOM.
 */
 
-const followersArray = [];
+const followersArray = [
+    'tetondan',
+    'dustinmyers',
+    'justsml',
+    'luishrd',
+    'bigknell'
+];
 
+followersArray.forEach(item => {
+  cardsFromArray(item)
+})
+
+function cardsFromArray(user){
+  axios.get(`https://api.github.com/users/${user}`)
+  .then(res => {
+    console.log(res.data)
+    const newCard = usercard(res.data)
+    console.log(newCard)
+    return newCard
+  })
+  .then(newCard => {
+    cards.appendChild(newCard)
+  })
+  .catch(err => console.log(err.message))
+  .finally(() => console.log('done'))
+}
 /*
   STEP 3: Create a function that accepts a single object as its only argument.
     Using DOM methods and properties, create and return the following markup:
@@ -61,44 +87,48 @@ const followersArray = [];
       </div>
     </div>
 */
-function usercard(avatar_url, name, login, location, html_url, followers, following, bio){
+function usercard ({avatar_url, name, login, location, html_url, followers, following, bio}) {
   const card = document.createElement('div')
-  const image = document.createElement('img')
-  const cardInfo = document.createElement('div')
+  const img = document.createElement('img')
+  const cardDiv = document.createElement('div')
   const nameH3 = document.createElement('h3')
   const username = document.createElement('p')
   const locationP = document.createElement('p')
   const profileP = document.createElement('p')
+  const url = document.createElement('a')
   const followersP = document.createElement('p')
   const followingP = document.createElement('p')
   const bioP = document.createElement('p')
 
-  card.appendChild(image)
-  card.appendChild(cardInfo)
-  cardInfo.appendChild(nameH3)
-  cardInfo.appendChild(username)
-  cardInfo.appendChild(locationP)
-  cardInfo.appendChild(profileP)
-  cardInfo.appendChild(followersP)
-  cardInfo.appendChild(followingP)
-  cardInfo.appendChild(bioP)
-
-  card.classList.add('card')
-  cardInfo.classList.add('card-info')
-  nameH3.classList.add('name')
-  username.classList.add('username')
-
-  image.src = avatar_url
   nameH3.textContent = `${name}`
   username.textContent = `${login}`
   locationP.textContent = `Location: ${location}`
-  profileP.href = html_url
-  profileP.textContent = `Profile: ${html_url}`
+  profileP.textContent = `Profile: `
+  url.textContent = html_url
   followersP.textContent = `Followers: ${followers}`
   followingP.textContent = `Following: ${following}`
   bioP.textContent = `Bio: ${bio}`
 
-return card
+  img.src = avatar_url
+  url.href = html_url
+
+  card.appendChild(img)
+  card.appendChild(cardDiv)
+  cardDiv.appendChild(nameH3)
+  cardDiv.appendChild(username)
+  cardDiv.appendChild(locationP)
+  cardDiv.appendChild(profileP)
+  cardDiv.appendChild(followersP)
+  cardDiv.appendChild(followingP)
+  cardDiv.appendChild(bioP)
+  profileP.appendChild(url)
+
+  card.classList.add('card')
+  cardDiv.classList.add('card-info')
+  nameH3.classList.add('name')
+  username.classList.add('username')
+
+    return card;
 }
 /*
   List of LS Instructors Github username's:
